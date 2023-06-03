11e7b43966553b45115c5a0209b6d999e7f4a4cc
kacper.sarama <k.sarama@crif.com>
2023-06-03 13:31:31 +0200

    Episode 131 complete

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const multer = require("multer");
 
-const Post = require("../models/post");
+const postController = require("../controllers/post");
+
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
@@ -9,7 +10,7 @@ const router = express.Router();
 const MIME_TYPE_MAP = {
   "image/png": "png",
   "image/jpeg": "jpg",
-  "image/jpg": "jpg"
+  "image/jpg": "jpg",
 };
 
 const storage = multer.diskStorage({
@@ -22,113 +23,20 @@ const storage = multer.diskStorage({
     cb(error, "backend/images");
   },
   filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
+    const name = file.originalname.toLowerCase().split(" ").join("-");
     const ext = MIME_TYPE_MAP[file.mimetype];
     cb(null, name + "-" + Date.now() + "." + ext);
-  }
+  },
 });
 
-router.post(
-  "",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename,
-      // Kacper
-      // creator: req.body.userId
-      creator: req.userData.userId
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: "Post added successfully",
-        post: {
-          ...createdPost,
-          id: createdPost._id
-        }
-      });
-    });
-  }
-);
+router.post("", checkAuth, multer({ storage: storage }).single("image"), postController.createPost);
 
-router.put(
-  "/:id",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      // Kacper
-      // creator: req.body.userId
-      creator: req.userData.userId
-    });
-    // console.log(post);
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-      console.log(result);
-      if (result.modifiedCount > 0) {
-        res.status(200).json({ message: "Update successful!" });
-      } else {
-        res.status(401).json({ message: "Not authorized!"})
-      }
-    });
-  }
-);
+router.put("/:id", checkAuth, multer({ storage: storage }).single("image"), postController.updatePost);
 
-router.get("", (req, res, next) => {
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1)).limit(pageSize)
-  }
-  postQuery
-    .then(documents => {
-      fetchedPosts = documents;
-      return Post.count();
-    }).then(count => {
-      res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: fetchedPosts,
-        maxPosts: count
-      });
-    });
-});
+router.get("", postController.getPosts);
 
-router.get("/:id", checkAuth, (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  });
-});
+router.get("/:id", checkAuth, postController.getPost );
 
-router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
-    console.log(result);
-    if (result.deletedCount > 0) {
-      res.status(200).json({ message: "Deletion successful!" });
-    } else {
-      res.status(401).json({ message: "Not authorized!"})
-    }
-  });
-});
+router.delete("/:id", checkAuth, postController.deletePost);
 
 module.exports = router;

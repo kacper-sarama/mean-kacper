3410f34fb99737dd3c7053c9e6daed2b819fe2a8
kacper.sarama <k.sarama@crif.com>
2023-06-04 19:47:59 +0200

    Episode 135 complete

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -39,7 +39,7 @@ exports.updatePost = (req, res, next) => {
   // console.log(post);
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
     console.log(result);
-    if (result.modifiedCount > 0) {
+    if (result.matchedCount > 0) {
       res.status(200).json({ message: "Update successful!" });
     } else {
       res.status(401).json({ message: "Not authorized!"})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,34 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
-import { PostListComponent } from "./posts/post-list/post-list.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { AuthInterceptor } from "./auth/auth-interceptor";
 import { AngularMaterialModule } from "./angular-material.module";
+import { PostsModule } from "./posts/posts.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
+
     HeaderComponent,
-    PostListComponent,
     LoginComponent,
     SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    PostsModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]

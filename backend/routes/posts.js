eae6b2191f7b4f1b139848f0e0e679fd405e336d
kacper.sarama <k.sarama@crif.com>
2023-06-04 19:14:28 +0200

    Episode 132 complete

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const multer = require("multer");
+const extractFile = require("../middleware/file");
 
 const postController = require("../controllers/post");
 
@@ -7,31 +7,9 @@ const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-};
+router.post("", checkAuth, extractFile, postController.createPost);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  },
-});
-
-router.post("", checkAuth, multer({ storage: storage }).single("image"), postController.createPost);
-
-router.put("/:id", checkAuth, multer({ storage: storage }).single("image"), postController.updatePost);
+router.put("/:id", checkAuth, extractFile, postController.updatePost);
 
 router.get("", postController.getPosts);
 
